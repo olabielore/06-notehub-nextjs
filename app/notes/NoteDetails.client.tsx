e375efc0f676deb846915a3a932153ebfe239ec0
Olabi Elore <olabielore@gmail.com>
2026-01-08 02:39:40 +0200

    my hw-06 next

---
+++ app/notes/NoteDetails.client.tsx
@@ -0,0 +1,40 @@
+
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from 'next/navigation';
+import { fetchNoteById } from "@/lib/api";
+import css from "./NoteDetails.module.css"
+
+const NoteDetailsClient = () => {
+	const { id } = useParams<{ id: string }>();
+
+  const { data: note, isLoading, error } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading, please wait...</p>;
+
+  if (error || !note) return <p>Something went wrong.</p>;
+
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
+
+  return (
+    <div className={css.container}>
+        <div className={css.item}>
+        <div className={css.header}>
+            <h2>Note title</h2>
+        </div>
+        <p className={css.content}>Note content</p>
+        <p className={css.date}>Created date</p>
+        </div>
+    </div>
+
+  );
+};
+
+export default NoteDetailsClient;
